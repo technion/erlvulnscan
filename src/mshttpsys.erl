689a52f3c5db372a9e19d644009ba51a62b2b224
Technion <technion@lolware.net>
2015-05-04 02:01:25 +0000

    Introduce network scan module, integrate jiffy json library

---
+++ src/mshttpsys.erl
@@ -0,0 +1,69 @@
+-module(mshttpsys).
+-compile(export_all).
+
+-define(TIMEOUT, 1000).
+-define(TESTHEADER, <<"GET / HTTP/1.1\r\nHost: stuff\r\nRange: bytes=0-18446744073709551615\r\n\r\n">>).
+-define(SCANSTR, "Requested Range Not Satisfiable").
+
+-spec mshttpsys_runscan(string()) -> 'ok'.
+mshttpsys_runscan(Network) ->
+	mshttpsys_spawner(254, Network),
+	mshttpsys_receive(254, []).
+
+-spec mshttpsys_receive(non_neg_integer(), _) -> any().
+mshttpsys_receive(0, Results) ->
+	Results;
+
+mshttpsys_receive(T, Results) ->
+	receive
+	{Address, Msg} ->
+		%io:fwrite("~s With ~w received address ~w~n", [Msg,Address,R]),
+		mshttpsys_receive(T-1, Results ++ [{Msg, Address}])
+	after ?TIMEOUT ->
+		Results
+	end.
+
+-spec mshttpsys_spawner(non_neg_integer(), _) -> 'ok'.
+mshttpsys_spawner(0, _) ->   
+	ok;
+
+mshttpsys_spawner(N, Network) ->
+	Pid = spawn(fun() ->
+		receive
+		{From, execute} ->
+			{ok, Address} = 
+			inet:parse_address(Network ++ integer_to_list(N)),
+			From ! {Address, mshttpsys(Address) }
+		end 
+	end),
+	Pid ! {self(), execute},
+	mshttpsys_spawner(N-1, Network).
+
+-spec mshttpsys({byte(),byte(),byte(),byte()}) -> 
+	'no_connection' | 'not_vulnerable' | 'vulnerable'.
+mshttpsys(Address) ->
+	%Known vulnerable: 212.48.69.194
+	case gen_tcp:connect(Address, 80, [], ?TIMEOUT) of
+	{ok, Socket} ->
+		gen_tcp:send(Socket, ?TESTHEADER),
+		inet:setopts(Socket, [{active, once}]),
+		receive
+		{tcp, Socket, Msg} ->
+			gen_tcp:close(Socket),
+			mshttpsys_scan(Msg)
+		after ?TIMEOUT ->
+			no_connection
+		end;
+	{error, _} ->
+		no_connection
+	end.
+
+-spec mshttpsys_scan(string()) -> 'not_vulnerable' | 'vulnerable'.
+mshttpsys_scan(Headers) ->
+	case string:str(Headers, ?SCANSTR) == 0 of
+	false ->
+		vulnerable;
+	true ->
+		not_vulnerable
+	end.
+
--- src/toppage_handler.erl
+++ src/toppage_handler.erl
@@ -12,16 +12,20 @@ init(_Transport, Req, []) ->
 
 handle(Req, State) ->
 	{Method, Req2} = cowboy_req:method(Req),
-	{Echo, Req3} = cowboy_req:qs_val(<<"network">>, Req2),
-	{ok, Req4} = network(Method, Echo, Req3),
+	{Network, Req3} = cowboy_req:qs_val(<<"network">>, Req2),
+	{ok, Req4} = network(Method, Network, Req3),
 	{ok, Req4, State}.
 
 network(<<"GET">>, undefined, Req) ->
 	cowboy_req:reply(400, [], <<"Missing network parameter.">>, Req);
-network(<<"GET">>, Echo, Req) ->
+network(<<"GET">>, _Network, Req) ->
+	Scan = mshttpsys:mshttpsys_runscan("127.0.0."),
+	%Convert erlang ip_address into string
+	ConvertFun = fun({X,Y}) -> {X,list_to_binary(inet:ntoa(Y))} end,
+	ScanConverted = lists:map(ConvertFun, Scan),
 	cowboy_req:reply(200, [
 		{<<"content-type">>, <<"text/plain; charset=utf-8">>}
-	], Echo, Req);
+	], jiffy:encode({ScanConverted}), Req);
 network(_, _, Req) ->
 	%% Method not allowed.
 	cowboy_req:reply(405, Req).
