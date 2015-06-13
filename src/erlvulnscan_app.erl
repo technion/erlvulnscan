f0355bf020aaeadade45262638f1e478208f9416
Technion <technion@lolware.net>
2015-06-13 08:00:55 +0000

    Implemented basic bootstrap UI Configured URL routing

--- src/erlvulnscan_app.erl
+++ src/erlvulnscan_app.erl
@@ -12,7 +12,7 @@
 start(_StartType, _StartArgs) ->
 	Dispatch = cowboy_router:compile([
 		{'_', [
-			{"/", toppage_handler, []}
+			{"/netscan", toppage_handler, []}
 		]}
 	]),
 	{ok, _} = cowboy:start_http(http, 100, [{port, 8080}], [
