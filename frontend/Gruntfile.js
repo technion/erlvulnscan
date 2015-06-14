3a03ffec6cd1e8a3bd9b2e162edc90229e04bbc5
Technion <technion@lolware.net>
2015-06-14 07:23:30 +0000

    Included HTML5 validator in build.

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -9,13 +9,22 @@ module.exports = function(grunt) {
     },
     jshint: {
         all: ['assets/erlvulnscan.jsx']
+    },
+    validation: {
+        options: {
+            relaxerror: ['This interface to HTML5 document checking is deprecated']
+        },
+        file: {
+        src: ['public/index.html']
+        }
     }
 
   });
 
   grunt.loadNpmTasks('grunt-react');
   grunt.loadNpmTasks('grunt-jsxhint');
+  grunt.loadNpmTasks('grunt-html-validation');
 
-  
-  grunt.registerTask('default', ['jshint', 'react']);
+ 
+  grunt.registerTask('default', ['jshint', 'react', 'validation']);
 };
