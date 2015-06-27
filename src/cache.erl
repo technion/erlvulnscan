9e527a3f72a529b86adbf6ce5bb9f2829b67805b
Technion <technion@lolware.net>
2015-06-27 08:32:01 +0000

    Add cache expire cleanup.

--- src/cache.erl
+++ src/cache.erl
@@ -1,6 +1,7 @@
 %% @doc Caching module. Uses ETS to cache cache output.
 -module(cache).
 -export([cached_fun/2, create_table/0, delete_table/0]).
+-include_lib("stdlib/include/ms_transform.hrl").
 
 %% @doc LIFETIME defines, in seconds, cache lifetime
 -ifdef(TEST).
@@ -25,12 +26,22 @@ now_secs() ->
     {MegaSecs, Secs, _MicroSecs} = os:timestamp(),
     MegaSecs*1000000 + Secs.
 
+%% @doc Expires old content
+-spec cache_flush() -> ok.
+cache_flush() ->
+    Now = now_secs(),
+    Selector = ets:fun2ms(fun({K, Expiry, _Res}) 
+            when Expiry < Now -> true end),
+    ets:select_delete(simple_cache, Selector),
+    ok.
+
 %% @doc For a key K, caches the output of F(K).
 -spec cached_fun(function(), list()) -> {_, 'expire' | 'hit' | 'miss'}.
 cached_fun(F, K) ->
     Now = now_secs(),
     case ets:lookup(simple_cache, K) of
     [] ->  %Not present in cache
+        cache_flush(), %Flushing on add ensures size is managed
         FuncRes = F(K),
         ets:insert(simple_cache, {K, Now+?LIFETIME, FuncRes}),
         {FuncRes, miss};
@@ -51,7 +62,8 @@ create_table_test() ->
 
 cached_fun_miss_test() ->
     F = fun(K) -> K ++ "demo" end, %A mock function
-    ?assertEqual(cached_fun(F, "127"), {"127demo", miss}).
+    ?assertEqual(cached_fun(F, "127"), {"127demo", miss}),
+    ?assertEqual(cached_fun(F, "cleanme"), {"cleanmedemo", miss}).
 cached_fun_insert_test() ->
     F = fun(K) -> K ++ "demo" end, %A mock function
     ?assertEqual(cached_fun(F, "127"), {"127demo", hit}).
@@ -59,6 +71,12 @@ cached_fun_expire_test() ->
     F = fun(K) -> K ++ "demo" end, %A mock function
     timer:sleep((?LIFETIME+1)*1000),
     ?assertEqual(cached_fun(F, "127"), {"127demo", expire}).
+cache_cleanup_test() ->
+    F = fun(K) -> K ++ "demo" end, %A mock function
+    ?assertEqual(cached_fun(F, "cleanme2"), {"cleanme2demo", miss}),
+    %The above insetion of a new value, afer the LIFETIME sleep ealier,
+    %should have cleaned up the earlier value below.
+    ?assertEqual(cached_fun(F, "cleanme"), {"cleanmedemo", miss}).
 
 delete_table_test() ->
     delete_table().
