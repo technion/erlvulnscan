7636334513be3540fb96a31e65c227e7c115d11a
Technion <technion@lolware.net>
2015-08-05 12:04:48 +0000

    Obey jshint recommendations about constructors.

--- frontend/assets/erlvulnscan.jsx
+++ frontend/assets/erlvulnscan.jsx
@@ -1,6 +1,6 @@
 require('./phantomjs-shims'); //Compatibility shims from Facbeook
 var React = require('../bower_components/react/react');
-var Swal = require('../bower_components/sweetalert/dist/sweetalert.min');
+var swal = require('../bower_components/sweetalert/dist/sweetalert.min');
 
 var NetscanList = React.createClass({
   render: function() {
@@ -49,7 +49,7 @@ var NetscanForm = React.createClass({
     }
     var re = /^\d+\.\d+\.\d+\.0$/; //IP Address match. Not a complete verifier.
     if (!network.match(re)) {
-        new Swal("Invalid input", "Please supply a valid network address in the form x.x.x.0", "error");
+        swal("Invalid input", "Please supply a valid network address in the form x.x.x.0", "error");
         return;
     }
     this.props.onNetscanSubmit(network);
@@ -88,7 +88,7 @@ var NetscanBox = React.createClass({
         React.findDOMNode(this.refs.prompt).innerHTML = "Scan and render completed in " + elapsed + "ms";
       }.bind(this),
       error: function(xhr, status, err) {
-        new Swal("Error", "Unable to connect to backend", "error");
+        swal("Error", "Unable to connect to backend", "error");
         console.error(this.props.url, status, err.toString());
       }.bind(this)
     });
