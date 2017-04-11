f9196eec861ba09489da17eb698bedc0289a3cb0
Technion <technion@lolware.net>
2017-04-11 12:30:47 +0000

    Introduced blacklist of scan ips.

--- src/ipmangle.erl
+++ src/ipmangle.erl
@@ -4,6 +4,15 @@
 
 -include("defs.hrl").
 
+-spec check_blacklist(byte()) -> ok.
+check_blacklist(A) ->
+    case A of
+    169 ->
+        erlang:error(blacklist_net);
+    _ ->
+        ok
+    end.
+
 %% @doc Verifies input is a valid network address. Converts binary input to
 %% list output. For example, binary "127.0.0.0" returns "127.0.0.".
 %% This is consistent with input required for the scanner.
@@ -15,7 +24,8 @@ verify_address(Network) ->
     NetworkList = binary_to_list(Network),
     % Verifies a valid address, and verifies it is a /24
     {ok, AddrTuple} = inet:parse_ipv4strict_address(NetworkList),
-    {_A, _B, _C, 0} = AddrTuple,
+    {A, _B, _C, 0} = AddrTuple,
+    check_blacklist(A),
     NetworkSubnet = lists:droplast(NetworkList),
     NetworkSubnet.
 
